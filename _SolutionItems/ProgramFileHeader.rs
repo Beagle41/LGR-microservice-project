9f09dbdf599ef099bd89c5de2e812b43f06e5991
Gary <TheDataBeagle@gmail.com>
2024-09-04 13:14:02 -0400

    Stage 1 Step 1
    Added files from LGR-problem plus added items to .gitignore to leave out various .vs whisker files

---
+++ _SolutionItems/ProgramFileHeader.rs
@@ -0,0 +1,21 @@
+/*
+ *	Title	    : &name.rs
+ *	Version	    : 0.0.1
+ *	Facility	: Rusty bootcamp.
+ *
+ *	Abstract	: Main program from bootcamp intermediate project: CLI
+ *					Purpose of project is to emulate limited subset of JIRA commands in
+ *					CLI program.
+ *
+ *	Envrionment	: Rust 1.76.0  (2021 edition according to Cargo.tom)
+ *
+ *	Author		: Gary Green (derived from Rusty bootcamp project)
+ *
+ *	History -- See git commit history for details.
+ *		 Version					Date                        Comment
+ *		  0.1.0                 18 July 2024                Using Rust's semantic version scheme:  Major.Minor.patch
+*/
+fn main() {
+    println!("Hello, world!");
+}
+
